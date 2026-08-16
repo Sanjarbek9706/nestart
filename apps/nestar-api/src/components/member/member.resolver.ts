a780b85763e1d032e2d6967c5cd715c223643c0f
Sanjarbek9706 <san19970618@gmail.com>
2026-08-16 19:47:47 +0900

    feat: develop logging via global interceptors

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,6 +1,5 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { MemberService } from './member.service';
-import { InternalServerErrorException } from '@nestjs/common';
 import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 import { Member } from '../../libs/dto/member/member';
 
@@ -19,10 +18,9 @@ export class MemberResolver {
 
   @Mutation(() => Member)
   public async login(@Args("input") input: LoginInput): Promise<Member> {
-   
       console.log('Mutation: login');
       return this.memberService.login(input);
-      
+
 }
 
   @Mutation(() => String)
--- apps/nestar-batch/src/main.ts
+++ apps/nestar-batch/src/main.ts
@@ -1,12 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { NestarBatchModule } from './nestar-batch.module';
-import { ValidationPipe } from '@nestjs/common';
-import { LoggingInterceptor } from '../../nestar-api/src/libs/interceptor/Logging.Interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(NestarBatchModule);
-  app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalInterceptors(new LoggingInterceptor());
   await app.listen(process.env.PORT_BATCH ?? 3000);
 }
 bootstrap();
+
