c5c1e6bf5ab7262525a99fcd13422e652c229638
Sanjarbek9706 <san19970618@gmail.com>
2026-09-04 01:15:04 +0900

    feat: create  subscribe, unsubscribe, getMemberFollowings and getMemberFollowers GraphQL APIs

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -50,3 +50,30 @@ export const lookupMember = {
 		as: 'memberData',
 	},
 };
+
+export const lookupMemberData = {
+	$lookup: {
+		from: 'members',
+		localField: 'memberId',
+		foreignField: '_id',
+		as: 'memberData',
+	},
+};
+
+export const lookupFollowingData = {
+	$lookup: {
+		from: 'members',
+		localField: 'followingId',
+		foreignField: '_id',
+		as: 'followingData',
+	},
+};
+
+export const lookupFollowerData = {
+	$lookup: {
+		from: 'members',
+		localField: 'followerId',
+		foreignField: '_id',
+		as: 'followerData',
+	},
+};
